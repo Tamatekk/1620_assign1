716a9e62cc6c400fdc3e83a2a66bcbabbfb51fbb
Tamatekk <rpi.irman@gmail.com>
2020-12-04 20:36:38 -0800

    Enter

--- apps.js
+++ apps.js
@@ -17,11 +17,16 @@ function change(){
   } else {
     x.innerHTML = "dark mode";
   }
-  if(top.style.background-color == rgb(167, 93, 93)) {
-    top.style.background-color == 'black';
-  } else {
-    top.style.background-color == 'red'
-  }
+    document.getElementById('top').style.backgroundColor = document.getElementById('top').style.backgroundColor === 'black' ? 'indianred' : 'black';
+    document.getElementById('Bottom').style.backgroundColor = document.getElementById('Bottom').style.backgroundColor === 'black' ? 'lightcoral' : 'black';
+    document.getElementById('side').style.backgroundColor = document.getElementById('side').style.backgroundColor === 'darkgrey' ? 'white' : 'darkgrey';
+    document.getElementById('Main').style.backgroundColor = document.getElementById('Main').style.backgroundColor === 'dimgrey' ? 'lightgrey' : 'dimgrey';
+    document.getElementById('t').style.backgroundColor = document.getElementById('t').style.backgroundColor === 'darkgrey' ? 'white' : 'darkgrey';
+    document.getElementById('Save').style.backgroundColor = document.getElementById('Save').style.backgroundColor === 'darkgrey' ? 'green' : 'darkgrey';
+    document.getElementById('h').style.backgroundColor = document.getElementById('h').style.backgroundColor === 'darkgrey' ? 'red' : 'darkgrey';
+    document.getElementById('Dark').style.backgroundColor = document.getElementById('Dark').style.backgroundColor === 'darkgrey' ? 'black' : 'darkgrey';
+    document.getElementById('r').style.backgroundColor = document.getElementById('r').style.backgroundColor === 'darkgrey' ? 'green' : 'darkgrey';
+
     }
 
     document.getElementById("h").addEventListener("click", hide)
@@ -61,33 +66,39 @@ function change(){
     
     function display0(){
     document.getElementById('output').innerHTML = JSON.stringify(Titles[0]);
+    document.getElementById('output').hidden = false;
     }
     
     document.getElementById('sidebar').children[2].addEventListener('click',display1);
     
     function display1(){
     document.getElementById('output').innerHTML = JSON.stringify(Titles[1]);
+    document.getElementById('output').hidden = false;
     }
     
     document.getElementById('sidebar').children[4].addEventListener('click',display2);
     
     function display2(){
     document.getElementById('output').innerHTML = JSON.stringify(Titles[2]);
+    document.getElementById('output').hidden = false;
     }
     document.getElementById('sidebar').children[6].addEventListener('click',display2);
     
     function display2(){
     document.getElementById('output').innerHTML = JSON.stringify(Titles[2]);
+    document.getElementById('output').hidden = false;
     }
     document.getElementById('sidebar').children[8].addEventListener('click',display2);
     
     function display2(){
     document.getElementById('output').innerHTML = JSON.stringify(Titles[2]);
+    document.getElementById('output').hidden = false;
     }
     document.getElementById('sidebar').children[10].addEventListener('click',display2);
     
     function display2(){
     document.getElementById('output').innerHTML = JSON.stringify(Titles[2]);
+    document.getElementById('output').hidden = false;
     }
     }
     
